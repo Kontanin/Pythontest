318baa4957e35ec88ab0b73b9b4348c086c8b79e
boss <boss12853@gmail.com>
2023-01-02 12:34:53 +0700

    modified:   .github/workflows/checks.yml 	modified:   app/core/management/commands/wait_for_db.py 	modified:   docker-compose.yml

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -4,7 +4,7 @@ Django command to wait for the database to be available.
 """
 
 import time
-from psycopg2 import OpearationalError as Psycopg20pError
+from psycopg2 import OperationalError as Psycopg20pError
 
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
