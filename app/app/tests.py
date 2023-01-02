d07f3783d0fe5a82fd4a235e67389c8691526f3a
boss <boss12853@gmail.com>
2023-01-02 14:07:24 +0700

    modified:   app/.flake8 	modified:   app/app/calc.py 	modified:   app/app/tests.py 	modified:   requirements.txt

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,13 +4,12 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     def test_add_number(self):
-        res =calc.add(5,6)
-        
-        self.assertEqual(res,11)
+        res =calc.add(5, 6)
+        self.assertEqual(res, 11)
         
     def test_subtract_number(self):
         # test subtractiong numbers. 
-        res =calc.subtract(10,15)
+        res =calc.subtract(10, 15)
         
-        self.assertEqual(res,5)
+        self.assertEqual(res, 5)
         
