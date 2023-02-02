8d778332ac4598ee625f88eae00140bb474371d5
boss <boss12853@gmail.com>
2023-02-02 20:36:46 +0700

    updates done by...

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,29 @@
-from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+class UserManager(BaseUserManager):
+    """ Manager for User """
+    def create_user(self,email,password=None,**extra_field):
+        """ Create"""
+        user =self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+        
+    
+class  User(AbstractBaseUser, PermissionsMixin):
+    email= models.EmailField(max_length=255, unique=True)
+    name = models.CharField( max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    object=UserManager()
+    
+    USERNAME_FIED = 'email'
+    
