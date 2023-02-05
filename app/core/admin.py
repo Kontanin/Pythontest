33dbcc20b522cd65e4254d922b4c79be0dabd936
boss <boss12853@gmail.com>
2023-02-05 16:25:26 +0700

    updates done by...

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+""" Django admin customization """
+
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+from core import  models
+
+
+class UserAdmmin(BaseUserAdmin):
+    ordering=['id']
+    list_display=['email','name']
+    
+    
+    
+admin.site.reqister(models.User, UserAdmin)
